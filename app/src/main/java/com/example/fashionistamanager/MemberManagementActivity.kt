1d20a9e0ba6d1fc60f4f7cbace2df40d1f17cd88
kimsung9112 <kimsung9112@gmail.com>
2021-10-09 16:29:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fashionistamanager/MemberManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fashionistamanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemberManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_management)
+    }
+}
